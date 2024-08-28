b6677f4a8b30fe359f852995cb67865336f51559
jettberg <164446552+jettberg@users.noreply.github.com>
2024-08-28 12:58:11 -0500

    For task 1.2- a simple nested array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
- let myName = 'Jett Berg';
- document.write(myName);
+let Charmander = {
+weight: "8.5 kg",
+height: "0.6 m",
+types: ["fire"],
+abilities: ["Blaze", "Solar-power"]
+};
 
- alert('Hello World');
+let Bulbasaur = {
+    weight: "6.9 kg",
+    height: "0.7 m",
+    types: ["grass", "poison"],
+    abilities: ["Chlorophyll", "Overgrow"]
+};
 
-let favoriteFood = "lasagna";
-document.write(favoriteFood);
+let Squirtle = {
+    weight: "9 kg",
+    height: "0.5 m",
+    types: ["water"],
+    abilities: ["Rain-dash", "Torrent"]
+};
+
+
+
+
+
+let pokemonList = [Charmander, Bulbasaur, Squirtle];
+alert(pokemonList);
