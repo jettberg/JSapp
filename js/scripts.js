9f5ddf54e4fed8fdd87986e2924b8f7fcf5a5959
jettberg <164446552+jettberg@users.noreply.github.com>
2024-09-10 22:44:27 -0500

    created an IIFE and stored the necessary data inside, corrected the forEach loop to match

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,53 @@ let Squirtle = {
     abilities: ["Rain-dash", "Torrent"]
 };
 
+let pokemonRepository = (function () {
+
+//im not really sure if all of this data needs to be stored in the IIFE? cause the code pulls, and works correctly when it is inside or out so im not sure which is better
+    // let Charmander = {
+    //     name: "Charmander",
+    //     weight: 8.5,
+    //     height: 0.6,
+    //     types: ["fire"],
+    //     abilities: ["Blaze", "Solar-power"]
+    // };
+    
+    // let Bulbasaur = {
+    //     name: "Bulbasaur",
+    //     weight: 6.9,
+    //     height: 0.7,
+    //     types: ["grass", "poison"],
+    //     abilities: ["Chlorophyll", "Overgrow"]
+    // };
+    
+    // let Squirtle = {
+    //     name: "Squirtle",
+    //     weight: 9,
+    //     height: 0.5,
+    //     types: ["water"],
+    //     abilities: ["Rain-dash", "Torrent"]
+    // };
+
+
+
 let pokemonList = [Charmander, Bulbasaur, Squirtle];
+  
+    function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+
+
 
 
 
@@ -33,11 +79,9 @@ let pokemonList = [Charmander, Bulbasaur, Squirtle];
 //         document.write("Wow " + pokemonList[i].name + " is tall!<br><br>");
 //     };
 // };
+// This was the previous code that was converted into the following:
 
-// This was the previous code
-
-
-pokemonList.forEach(listHeight);
+pokemonRepository.getAll().forEach(listHeight);
 
 function listHeight(item) {
     document.write (item.name + ", Height: " + item.height + " <br> <br>")
