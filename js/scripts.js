78fea3d404acbd392fb765ed3b1287b6f8a7d1e6
jettberg <164446552+jettberg@users.noreply.github.com>
2024-09-16 14:18:06 -0500

    put pokemon data inside the IIFE, tweaked a couple functions to list the pokemon, changed some UI, added a click-event on buttons:

--- js/scripts.js
+++ js/scripts.js
@@ -1,90 +1,83 @@
-let Charmander = {
+
+
+let pokemonRepository = (function () {
+
+  let Charmander = {
     name: "Charmander",
     weight: 8.5,
     height: 0.6,
     types: ["fire"],
     abilities: ["Blaze", "Solar-power"]
-};
+  };
 
-let Bulbasaur = {
+  let Bulbasaur = {
     name: "Bulbasaur",
     weight: 6.9,
     height: 0.7,
     types: ["grass", "poison"],
     abilities: ["Chlorophyll", "Overgrow"]
-};
+  };
 
-let Squirtle = {
+  let Squirtle = {
     name: "Squirtle",
     weight: 9,
     height: 0.5,
     types: ["water"],
     abilities: ["Rain-dash", "Torrent"]
-};
-
-let pokemonRepository = (function () {
-
-//im not really sure if all of this data needs to be stored in the IIFE? cause the code pulls, and works correctly when it is inside or out so im not sure which is better
-    // let Charmander = {
-    //     name: "Charmander",
-    //     weight: 8.5,
-    //     height: 0.6,
-    //     types: ["fire"],
-    //     abilities: ["Blaze", "Solar-power"]
-    // };
-    
-    // let Bulbasaur = {
-    //     name: "Bulbasaur",
-    //     weight: 6.9,
-    //     height: 0.7,
-    //     types: ["grass", "poison"],
-    //     abilities: ["Chlorophyll", "Overgrow"]
-    // };
-    
-    // let Squirtle = {
-    //     name: "Squirtle",
-    //     weight: 9,
-    //     height: 0.5,
-    //     types: ["water"],
-    //     abilities: ["Rain-dash", "Torrent"]
-    // };
+  };
 
 
+  let pokemonList = [Charmander, Bulbasaur, Squirtle];
 
-let pokemonList = [Charmander, Bulbasaur, Squirtle];
+  function add(pokemon) {
+
+    //   if(
+    //     typeof pokemon === "object" &&
+    //     "name" in pokemon &&
+    //     "types" in pokemon &&
+    //     "height" in pokemon &&
+    //     "weight" in pokemon &&
+    //     "abilities" in pokemon 
+    //   ){
+    //   pokemonRepository.push(pokemon);
+    // } else {
+    //   console.log("The pokemon is not correct");
+    // 
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+
+  function addListItem(pokemon) {
+    let pokemonli = document.querySelector(".pokemon-list");
+    let liItem = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button");
+    button.addEventListener('click', function showDetails(pokemon) {
+      console.log(pokemon.name);
+    });
+
+    liItem.appendChild(button);
+    pokemonli.appendChild(liItem);
+  }
+
   
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-  
-    function getAll() {
-      return pokemonList;
-    }
-  
-    return {
-      add: add,
-      getAll: getAll
-    };
-  })();
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
 
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
+  };
+})();
 
-
-
-
-
-
-// for (let i = 0; i < pokemonList.length; i++) {
-//     document.write(pokemonList[i].name + ", Height: ", pokemonList[i].height + ", <br><br>");
-//     if (pokemonList[i].height > 0.6) {
-//         document.write("Wow " + pokemonList[i].name + " is tall!<br><br>");
-//     };
-// };
-// This was the previous code that was converted into the following:
-
-pokemonRepository.getAll().forEach(listHeight);
-
-function listHeight(item) {
-    document.write (item.name + ", Height: " + item.height + " <br> <br>")
-    if (item.height > 0.6) {
-    document.write ("Wow " + item.name +" is tall!<br><br>")
-}}
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon)
+});
